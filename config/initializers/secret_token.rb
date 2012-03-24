5ed7dc3dfd93596ec9ff99c75b9dae83b0514dab
Dimitri Kurashvili <dimitri@c12.ge>
2012-03-24 15:28:02 +0400

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExpressTelasiGe::Application.config.secret_token = '6d07efb7c13623edac64f69a8fe6d730ad0b36f8f6ee8e3aea2596748711d1f01e9bb39662182cb13d8decf9870d5551b279d31c4b7c640da0fd8ca31596153c'
