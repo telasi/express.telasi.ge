0776fafb7274874d1c8a9ef1025a185576afa407
Dimitri Kurashvili <dimitri@c12.ge>
2012-03-25 13:34:26 +0400

    adding login button on home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # -*- encoding : utf-8 -*-
 ExpressTelasiGe::Application.routes.draw do
   get '/home', :controller => :site, :action => :index
+  match '/login', :controller => :site, :action => :login
+  get '/logout', :controller => :site, :action => :logout
 
   root :to => 'site#index'
 end
